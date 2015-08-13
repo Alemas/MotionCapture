91bda83513e44080bcbd7571d1a72081f3ae9fd7
Alemas <mateusreckziegel@gmail.com>
2015-08-13 17:20:20 -0300

    first

--- MotionCapture/ConnectionManager.swift
+++ MotionCapture/ConnectionManager.swift
@@ -7,7 +7,102 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ConnectionManager: NSObject {
+protocol ConnectionManagerDelegate {
+    
+    func didConnectToDevice(connectionManager:ConnectionManager, device:String)
+    
+}
+
+class ConnectionManager:NSObject {
    
+    private let serviceType = "motion-app"
+    private let myPeerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+    private let advertiser:MCNearbyServiceAdvertiser
+    var delegate:ConnectionManagerDelegate?
+    lazy var session:MCSession = {
+        
+        let session = MCSession(peer: self.myPeerID, securityIdentity: nil, encryptionPreference: MCEncryptionPreference.Required)
+        session?.delegate = self
+        return session
+        
+    }()
+    
+    override init() {
+        
+        self.advertiser = MCNearbyServiceAdvertiser(peer: self.myPeerID, discoveryInfo: nil, serviceType: self.serviceType)
+        super.init()
+        self.advertiser.delegate = self
+        self.advertiser.startAdvertisingPeer()
+        
+    }
+    
+    func sendData(data:NSData!){
+        
+        if self.session.connectedPeers.count > 0 {
+            var error:NSError?
+            self.session.sendData(data, toPeers: self.session.connectedPeers, withMode: MCSessionSendDataMode.Reliable, error: &error)
+            if error != nil {
+                println("Houston we've got a problem (sendData)")
+            }
+        }
+    }
+}
+
+extension MCSessionState {
+    
+    func stringValue() -> String {
+        switch(self) {
+        case .NotConnected: return "NotConnected"
+        case .Connecting: return "Connecting"
+        case .Connected: return "Connected"
+        default: return "Unknown"
+        }
+    }
+    
+}
+
+extension ConnectionManager : MCSessionDelegate {
+    
+    func session(session: MCSession!, peer peerID: MCPeerID!, didChangeState state: MCSessionState) {
+        println("\(state.stringValue())")
+        
+        switch state {
+        case .Connected:
+            self.delegate?.didConnectToDevice(self, device: session.connectedPeers.map({$0.displayName})[0] as String)            
+        default:
+            print("")
+        }
+    }
+    
+    func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
+        
+    }
+    
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
+        
+    }
+    
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
+        
+    }
+    
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
+        
+    }
+    
+    func session(session: MCSession!, didReceiveCertificate certificate: [AnyObject]!, fromPeer peerID: MCPeerID!, certificateHandler: ((Bool) -> Void)!) {
+        certificateHandler(true)
+    }
+    
+}
+
+extension ConnectionManager : MCNearbyServiceAdvertiserDelegate {
+    
+    func advertiser(advertiser: MCNearbyServiceAdvertiser!, didReceiveInvitationFromPeer peerID: MCPeerID!, withContext context: NSData!, invitationHandler: ((Bool, MCSession!) -> Void)!) {
+        println("didReceiveInvitationFromPeer: \(peerID)")
+        invitationHandler(true, self.session)
+    }
+    
 }
--- MotionCapture/ViewController.swift
+++ MotionCapture/ViewController.swift
@@ -7,19 +7,89 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    var active = false
+    let connectionManager = ConnectionManager()
+    let cmManager = CMMotionManager()
+    var motionTrack = false
+    var motionQueue = NSOperationQueue.mainQueue()
+    var c = 0
+    
+    @IBOutlet weak var lblTime: UILabel!
+    
+    @IBOutlet weak var sldTime: UISlider!
+    
+    @IBOutlet weak var lblData: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.connectionManager.delegate = self
+        self.sldTime.maximumValue = 1.0
+        self.sldTime.minimumValue = 0.01
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
+    @IBAction func didPressSendData(sender: AnyObject) {
+        let btn = sender as! UIButton
+        self.active = !self.active
+        
+        
+        if self.active {
+            
+            self.cmManager.deviceMotionUpdateInterval = NSTimeInterval(self.sldTime.value)
+            
+            self.cmManager.startDeviceMotionUpdatesToQueue(self.motionQueue, withHandler: {(data, error) in
+            
+                let a = data.userAcceleration
+                let acc = [Float(self.c), Float(a.x), Float(a.y), Float(a.z)]
+                
+                let g = data.attitude
+                let rot = [Float(g.roll), Float(g.pitch), Float(g.yaw)]
+                
+                println(rot)
+                
+//                var length:Int = 0
+//                
+//                for i in acc {
+//                    length = length + sizeof(i.dynamicType)
+//                }
+                
+                let data = NSData(bytes: acc, length: 16)
+                
+                self.connectionManager.sendData(data)
+            
+                self.lblData.text = String(format: "%d X %.4f  |  Y %.4f  |  Z %.4f", arguments: [self.c, a.x, a.y, a.z])
+                self.c++
+            })
+
+            btn.setTitle("Stop sending motion data", forState: UIControlState.Normal)
+            
+        } else {
+            self.c = 0
+            self.cmManager.stopDeviceMotionUpdates()
+            btn.setTitle("Send motion data", forState: UIControlState.Normal)
+        }
+        
+    }
+    
+    @IBAction func didMoveSlider(sender: UISlider) {
+        self.lblTime.text = "\(sender.value)s"
+    }
 
 }
 
+extension ViewController : ConnectionManagerDelegate {
+    
+    func didConnectToDevice(connectionManager: ConnectionManager, device: String) {
+        
+    }
+    
+}
